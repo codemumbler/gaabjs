a19798aa330fb36e4b11d6b3f9bd8eed63eea14c
Steven Armstrong <codemumbler@users.noreply.github.com>
2015-09-23 21:55:20 -0400

    creates cookie to save value

--- js/ab.js
+++ js/ab.js
@@ -9,9 +9,20 @@
 		for(var i=0; i<ca.length; i++) {
 			var c = ca[i];
 			while (c.charAt(0)==' ') c = c.substring(1);
-				if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+				if (c.indexOf(name) == 0) {
+					var value = c.substring(name.length,c.length);
+					if (value)
+						return value;
+				}
 		}
 		return null;
+	};
+
+	function setCookie(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = cname + "=" + cvalue + "; " + expires;
 	};
 
 	var getIndex = function() {
@@ -27,6 +38,7 @@
 
 	var renderExperiment = function(index) {
 		$this.text(experiments[index].value);
+		setCookie(COOKIE_NAME, index, 1);
 	};
 
 	$.fn.ab = function(options) {
--- test/ab.Spec.js
+++ test/ab.Spec.js
@@ -1,5 +1,9 @@
 describe('AB Testing', function() {
 
+	afterEach(function() {
+		document.cookie = "codemumblerAB_index=;";
+	});
+
 	describe('AB plugin', function() {
 		it('jquery plugin', function() {
 			expect(typeof $.fn.ab).toBe('function');
@@ -17,9 +21,8 @@ describe('AB Testing', function() {
 			$(document.body).append("<div class='test-ab-element'/>");
 			testElement = $('.test-ab-element');
 			Math.random = function() {
-				return randomValue();
+				return 0;
 			};
-			randomValue = function(){ return 0 };
 		});
 
 		afterEach(function(){
@@ -47,7 +50,9 @@ describe('AB Testing', function() {
 		});
 
 		it('define 2 text alternatives - pick B', function() {
-			randomValue = function(){ return 0.9 };
+			Math.random = function() {
+				return 0.9;
+			};
 			testElement.ab({
 				'experiments':[{
 					'value': 'experiment A'
@@ -59,34 +64,66 @@ describe('AB Testing', function() {
 		});
 	});
 
-	describe('Experiment survives refresh via cookie', function() {
+	describe('Cookie tests - ', function() {
 		var testElement;
+		function readCookie(cname) {
+			var name = cname + "=";
+			var ca = document.cookie.split(';');
+			for(var i=0; i<ca.length; i++) {
+				var c = ca[i];
+				while (c.charAt(0)==' ') c = c.substring(1);
+					if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+			}
+			return null;
+		};
+
 		function setCookie(cname, cvalue, exdays) {
 			var d = new Date();
 			d.setTime(d.getTime() + (exdays*24*60*60*1000));
 			var expires = "expires="+d.toUTCString();
 			document.cookie = cname + "=" + cvalue + "; " + expires;
-		}
+		};
+
 		beforeEach(function(){
 			$(document.body).append("<div class='test-ab-element'/>");
 			testElement = $('.test-ab-element');
-			setCookie('codemumblerAB_index', '0', 1);
 		});
 
 		afterEach(function(){
 			$('.test-ab-element').remove();
-			setCookie('codemumblerAB_index', '0', 0);
 		});
 
-		it('Uses cookie value', function() {
-			testElement.ab({
-				'experiments':[{
-					'value': 'experiment A'
-				},{
-					'value': 'experiment B'
-				}]
+		describe('Experiment survives refresh via cookie', function() {
+			beforeEach(function(){
+				setCookie('codemumblerAB_index', '0', 1);
 			});
-			expect(testElement.text()).toBe('experiment A');
+
+			it('Uses cookie value', function() {
+				testElement.ab({
+					'experiments':[{
+						'value': 'experiment A'
+					},{
+						'value': 'experiment B'
+					}]
+				});
+				expect(testElement.text()).toBe('experiment A');
+			});
+		});
+
+		describe('Saves experiment index', function() {
+			it('Creates cookie', function() {
+				Math.random = function() {
+					return 0.9;
+				};
+				testElement.ab({
+					'experiments':[{
+						'value': 'experiment A'
+					},{
+						'value': 'experiment B'
+					}]
+				});
+				expect(readCookie('codemumblerAB_index')).toBe('1');
+			});
 		});
 	});
 });
