69561cf4058ad5da196b2c5f1abe9fb81133af4f
Steven Armstrong <steven.armstrong@gmail.com>
2016-07-26 21:12:48 -0400

    config to test in multiple browser

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
+    browsers: ['Firefox','Chrome'],
     singleRun: false
   });
 };
